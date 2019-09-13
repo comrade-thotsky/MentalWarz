9f38cda26ac390030a8dd8eadbe496f4589322fa
Smith-Perkins <1662978@fcps.edu>
2019-09-13 15:13:19 -0400

    Added Player Class
    My computer is gonna die, so I'm gonna restructure the files at home lol.

---
+++ Test.js
@@ -0,0 +1,13 @@
+import Player from "/src/Player";
+document.addEventListener("keydown", function(event) {
+  console.log(event.which);
+});
+let canvas = document.getElementById("gameScreen");
+let c = canvas.getContext("2d");
+var PlayerX = 20;
+var PlayerY = 20;
+const CanvasWidth = 800;
+const CanvasHeight = 600;
+
+c.fillStyle = "red";
+c.fillRect(20, 20, 100, 100);
