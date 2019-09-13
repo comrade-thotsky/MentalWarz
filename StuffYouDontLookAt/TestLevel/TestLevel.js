5920fa37c90777551214d440b8046a16256deca8
IAmBot102102 <BotNumber102102@gmail.com>
2019-09-13 17:32:51 -0400

    added rudimentary controls

--- StuffYouDontLookAt/TestLevel/TestLevel.js
+++ StuffYouDontLookAt/TestLevel/TestLevel.js
@@ -9,7 +9,46 @@ document.addEventListener("keydown", function(event){
     if(event.which == 87){
         c.clearRect(x,y,size,size);
         c.fillStyle = color;
+        y -= 20;
+        if(y <= 0){
+            y = 0;
+        }
+        c.fillRect(x,y,size,size);
+    }
+    if(event.which == 65){
+        c.clearRect(x,y,size,size);
+        c.fillStyle = color;
         x -= 20;
+        if(x <= 0){
+            x = 0;
+        }
+        c.fillRect(x,y,size,size);
+    }
+    if(event.which == 83){
+        c.clearRect(x,y,size,size);
+        c.fillStyle = color;
+        y += 20;
+        if(y >= 600){
+            y = 600;
+        }
+        c.fillRect(x,y,size,size);
+    }
+    if(event.which == 68){
+        c.clearRect(x,y,size,size);
+        c.fillStyle = color;
+        x += 20;
+        if(x >= 800){
+            x = 800;
+        }
+        c.fillRect(x,y,size,size);
+    }
+    if(event.which == 32){
+        c.clearRect(x,y,size,size);
+        c.fillStyle = color;
+        x -= 20;
+        if(x <= 0){
+            x = 0;
+        }
         c.fillRect(x,y,size,size);
     }
 })
