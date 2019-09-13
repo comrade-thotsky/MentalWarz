ad50ff5b2ec5902c2f45747bf889532bc7190226
IAmBot102102 <BotNumber102102@gmail.com>
2019-09-13 16:56:22 -0400

    Added Test js

--- StuffYouDontLookAt/TestLevel/TestLevel.js
+++ StuffYouDontLookAt/TestLevel/TestLevel.js
@@ -2,4 +2,12 @@ import Player from "../GameMechanics/Player"
 let canvas = document.getElementById("gameScreen");
 let c = canvas.getContext("2d");
 
-Player.draw(c);
+function DrawPlayer(PlayerX, PlayerY, PlayerSize, PlayerColor){
+    c.fillStyle = PlayerColor;
+    c.fillRect(PlayerX, PlayerY, PlayerSize, PlayerSize);
+}
+var PlayerX = 20;
+var PlayerY = 20;
+var PlayerSize = 100;
+var PlayerColor = "red";
+DrawPlayer(PlayerX, PlayerY, PlayerSize, PlayerColor);
