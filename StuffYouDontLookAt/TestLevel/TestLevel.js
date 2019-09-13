dab92f9ce4bf9f2e98441f4ba2208049a27c637b
IAmBot102102 <BotNumber102102@gmail.com>
2019-09-13 17:12:27 -0400

    ADDED PLAYER

---
+++ StuffYouDontLookAt/TestLevel/TestLevel.js
@@ -0,0 +1,8 @@
+let canvas = document.getElementById("gameScreen");
+let c = canvas.getContext("2d");
+var x = 20;
+var y = 20;
+var size = 100;
+var color = "red";
+c.fillStyle = color;
+c.fillRect(x,y,size,size);
