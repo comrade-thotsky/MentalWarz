dd17e256127efd09fd762867c822eff451319d17
IAmBot102102 <BotNumber102102@gmail.com>
2019-09-13 17:24:16 -0400

    added something i might regret

--- StuffYouDontLookAt/TestLevel/TestLevel.js
+++ StuffYouDontLookAt/TestLevel/TestLevel.js
@@ -4,5 +4,15 @@ var x = 20;
 var y = 20;
 var size = 100;
 var color = "red";
+document.addEventListener("keydown", function(event){
+    console.log(event.which);
+    if(event.which == 87){
+        c.clearRect(x,y,size,size);
+        c.fillStyle = color;
+        x -= 20;
+        c.fillRect(x,y,size,size);
+    }
+})
+
 c.fillStyle = color;
 c.fillRect(x,y,size,size);
