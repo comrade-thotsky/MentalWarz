53462d7344d5491b6aa88086583075f06546eda4
IAmBot102102 <BotNumber102102@gmail.com>
2019-09-15 17:43:41 -0400

    Restructured and Added Moving platform

--- StuffYouDontLookAt/TestLevel/TestLevel.js
+++ StuffYouDontLookAt/TestLevel/TestLevel.js
@@ -1,9 +1,9 @@
 let canvas = document.getElementById("gameScreen");
 let c = canvas.getContext("2d");
-var size = 50;
-var x = 0;
-var y = 600-size;
-var color = "red";
+var size = 20;
+var position = [0,1];
+var Obsticals = [[5,10,2,2,"dynamicx", 5, 10, "up"]];
+var color = "blue";
 var jump = false;
 var left = false;
 var right = false;
@@ -12,83 +12,76 @@ document.addEventListener("keydown", function(event){
     // w
     if(event.which == 87){
         if(jump == false){
-            c.clearRect(x,y,size,size);
-            c.fillStyle = color;
-            y -= 100;
-            if(y <= 0){
-                y = 0;
-            }
-            c.fillRect(x,y,size,size);
             jump = true;
         }
     }
     //a
     if(event.which == 65){
-        left = true;
-        right = false;
-        c.clearRect(x,y,size,size);
-        c.fillStyle = color;
-        x -= 20;
-        if(x <= 0){
-            x = 0;
-        }
-        c.fillRect(x,y,size,size);
+
     }
     //d
     if(event.which == 68){
-        left = false;
-        right = true;
-        c.clearRect(x,y,size,size);
-        c.fillStyle = color;
-        x += 20;
-        if(x >= 800 - size){
-            x = 800 - size;
-        }
-        c.fillRect(x,y,size,size);
     }
     //space
     if(event.which == 32){
         if(jump == false){
-            c.clearRect(x,y,size,size);
-            c.fillStyle = color;
-            y -= 100;
-            if(y <= 0){
-                y = 0;
-            }
-            c.fillRect(x,y,size,size);
-            jump = true;
-            if(left == true){
-                x -= 20;
-                if(x <= 0){
-                    x = 0;
-                }
-                c.fillRect(x,y,size,size);
-            }
-            if(right == true){
-                x += 20;
-                if(x <= 800-size){
-                    x = 800-size;
-                }
-                c.fillRect(x,y,size,size);
-            }
+
         }
     }
 })
 function PlayerGravity(){
-    y += 20;
-    if(y >= 600-size){
-        y = 600-size;
-        jump = false;
+    position[1] += 1;
+    if(position[1] >= 30){
+        position[1] -= 1;
     }
+    DrawPlayer();
 }
 function DrawPlayer(){
     c.fillStyle = color;
-    c.fillRect(x,y,size,size);
+    c.fillRect(position[0]*20, position[1]*20, size, size);
+}
+function ClearObstacles(){
+    Obsticals.forEach(Ob => {
+        c.clearRect(Ob[0]*20, Ob[1]*20, Ob[2]*20, Ob[3]*20);  
+    });
+}
+function DrawObstacles(){
+    ClearObstacles();
+    Obsticals.forEach(Ob => {
+        if(Ob[4] == "static"){
+            var ObColor = "purple";
+        }
+        if(Ob[4] == "dynamicx"){
+            var ObColor = "blue";
+            if(Ob[7] == "up"){
+                Ob[0] += 1;
+            }
+            if(Ob[7] == "down"){
+                Ob[0] -= 1;
+            }
+            if(Ob[0] >= Ob[6]){
+                Ob[7] = "down";
+            }
+            if(Ob[0] <= Ob[5]){
+                Ob[7] = "up";
+            }
+        }
+        if(Ob[4] == "dynamicy"){
+            var ObColor = "orange";
+            Ob[0] += 1;
+        }
+        c.fillStyle = ObColor;
+        c.fillRect(Ob[0]*20, Ob[1]*20, Ob[2]*20, Ob[3]*20);
+
+    });
+}
+function ErasePlayer(){
+    c.clearRect(position[0]*20, position[1]*20, size, size);
 }
 function Game(){
-    c.clearRect(x,y,size,size);
+    ErasePlayer();
     PlayerGravity();
-    c.clearRect(x,y,size,size);
     DrawPlayer();
+    DrawObstacles();
 }
-var interval = setInterval(Game, 75);
+var interval = setInterval(Game, 60);
