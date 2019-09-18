b1a64145395243185aaa075ad06717183a2989b5
comrade-thotsky <53873860+comrade-thotsky@users.noreply.github.com>
2019-09-18 09:32:00 -0400

    Create content.js

---
+++ Main Page/content.js
@@ -0,0 +1,9 @@
+function Play(){
+    location.href = "StuffYouDontLookAt/LevelOne/LevelOne.html";
+}
+function Options(){
+    location.href = "StuffYouDontLookAt/MainScreen/Options.html";
+}
+function Credits(){
+    location.href = "StuffYouDontLookAt/MainScreen/Credits.html";
+}
